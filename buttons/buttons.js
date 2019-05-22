e56663a77cd5f9f89a1aef4f678917b13280d638
Matt Locklin <mattlocklin@live.co.uk>
2019-05-22 12:28:13 +0100

    Latest version with sliders working

---
+++ buttons/buttons.js
@@ -0,0 +1,13 @@
+// const focused = document.querySelectorAll('button')
+
+var x = document.getElementById("act1");
+x.addEventListener("focusin", console.log(`1`), false);
+
+var x = document.getElementById("act2");
+x.addEventListener("focusin", console.log(`2`), false);
+
+var x = document.getElementById("act3");
+x.addEventListener("focusin", console.log(`3`), false);
+
+var x = document.getElementById("act4");
+x.addEventListener("focusin", console.log(`4`), false);
